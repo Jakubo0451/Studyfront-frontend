dde7878e1d0af833c67e0b7435a8da0367c7118c
TheRealJensJK <jensnic@stud.ntnu.no>
2025-03-07 15:41:30 +0100

    Added back font

--- research-app/src/app/layout.js
+++ research-app/src/app/layout.js
@@ -14,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-ice-blue">
+      <body className="bg-ice-blue font-kaisei">
         {children}
       </body>
     </html>
