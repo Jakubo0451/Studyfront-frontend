b598831ce1a0d4a13a60612a55ad01bc15fd3692
Jens <jens@diri.no>
2025-03-11 13:03:50 +0100

    Enhance homepage layout with logo and updated call-to-action links

--- research-app/src/app/page.js
+++ research-app/src/app/page.js
@@ -1,11 +1,24 @@
 import Link from 'next/link';
+import Image from "next/image";
+import logo from "./../../public/logo/logo.png";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Create studies, conduct research</h1>
-      <Link href="/login">Log in</Link>
-      <Link href="/signup">Sign Up</Link>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <div className="w-80 space-y-8 flex flex-col items-center justify-center">
+        <Image
+            className="flex justify-center items-center"
+            src={logo}
+            alt="Logo"
+            height={50}
+            layout="intrinsic"
+          />
+        <h1 className="flex flex-col items-center justify-center text-center text-2xl">Simple and powerful research tool for your studies</h1>
+        <div className="w-full flex flex-grow space-x-4 mb-2">
+          <Link href={`/edit/`} className="bg-petrol-blue text-white rounded px-4 py-2 flex-grow text-center">Edit</Link>
+          <Link href={`/share`} className="bg-petrol-blue text-white rounded px-4 py-2 flex-grow text-center">Share</Link>
+        </div>
+      </div>
     </div>
   );
 }
