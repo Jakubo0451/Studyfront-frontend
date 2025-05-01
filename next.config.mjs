2e6a58dd98653616adede332ec7467970978ba26
Jens <jens@diri.no>
2025-05-01 16:49:12 +0200

    feat: add .env.example, update dotenv version, and enhance CI workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
+import backendUrl from './environment.js';
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
+
+console.log(`Using backend: ${backendUrl}`);
 
 export default nextConfig;
